951d962253ca24760ffd67e2477cb8b9149bdf80
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2024-01-08 16:35:51 +0000

    Align strings

--- Code/Models/ROCV/retrieve_params.m
+++ Code/Models/ROCV/retrieve_params.m
@@ -29,15 +29,15 @@ end
 
 if verbose && isstruct(sol)
     % Display the parameters
-    disp(['Qn = ' num2str(Qn/hr) '*hr; % negative electrode capacity (As)']);
-    disp(['nu = ' num2str(nu) ';       % negative-positive electrode capacity ratio (non-dim.)']);
-    disp(['miu = ' num2str(miu) ';     % cyclable lithium-positive electrode capacity ratio (non-dim.)']);
-    disp(['Rs = ' num2str(Rs) ';       % series resistance (Ohm)']);
+    disp([pad(['Qn = ' num2str(Qn/hr) '*hr;'], 21) '% negative electrode capacity (As)']);
+    disp([pad(['nu = ' num2str(nu) ';'], 21)       '% negative/positive electrode capacity ratio (non-dim.)']);
+    disp([pad(['miu = ' num2str(miu) ';'], 21)     '% cyclable lithium/positive electrode capacity ratio (non-dim.)']);
+    disp([pad(['Rs = ' num2str(Rs) ';'], 21)       '% series resistance (Ohm)']);
     disp( ' '); disp('% Update capacity');
-    disp(['CE = ' num2str(CE) ';       % coulombic efficiency (non-dim.)']);
+    disp([pad(['CE = ' num2str(CE) ';'], 21)       '% coulombic efficiency (non-dim.)']);
     
     % Display initial states
-    disp(['S0 = ' num2str(S0) '; % initial SOC']);
+    disp([pad(['S0 = ' num2str(S0) ';'], 21)       '% initial SOC']);
 end
 
 
